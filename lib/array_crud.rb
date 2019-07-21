31b6f5f3a5dfa013a13e58a85f5b0d931ff3ab4d
Zermina Ejaz <zerminaejaz1018@icloud.com>
2019-07-21 03:10:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,7 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  array[array.length - 1]
+  array[-1]
 end
 
 def update_element_from_index(array, index_number, element)
